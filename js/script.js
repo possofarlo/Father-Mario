9e284148df57af2eb713922925e110fb7bc426be
possofarlo <wagasiaji2gmail.com>
2025-10-15 12:51:13 +0300

    aain changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 
 console.log("Father Mario Foundation – Watamu");
 
+// Make profile image open in lightbox when clicked
+document.querySelector('.profile-circle img').addEventListener('click', function(e) {
+  e.preventDefault();
+  const modal = document.getElementById('lightbox-modal');
+  const modalImg = document.getElementById('lightbox-img');
+  modalImg.src = this.src;
+  modal.classList.add('show');
+});
+
+// Existing lightbox code for gallery/about images
 document.querySelectorAll('.lightbox').forEach(link => {
   link.addEventListener('click', function(e) {
     e.preventDefault();
